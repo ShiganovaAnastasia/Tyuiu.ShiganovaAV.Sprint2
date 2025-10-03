9927a32fea0efe1d91cde7454360ca6759b6595f
clounada <Tyuiu.ShiganovaAV@outlook.com>
2025-10-03 20:41:05 +0300

    .

--- Tyuiu.ShiganovaAV.Sprint2.Task2.V5.Lib/DataService.cs
+++ Tyuiu.ShiganovaAV.Sprint2.Task2.V5.Lib/DataService.cs
@@ -5,17 +5,21 @@ namespace Tyuiu.ShiganovaAV.Sprint2.Task2.V5.Lib
     {
         public bool CheckDotInShadedArea(int x, int y)
         {
-            if (y >= 3 && y <= 5 && x >= 2 && x <= 4)
+            if ((y == 3 || y == 4 || y == 5) && (x >= 2 && x <= 4))
                 return true;
-            if (y >= 4 && y <= 10 && x == 5)
+            if ((y == 3 || y == 4 || y == 5) && (x >= 10 && x <= 12))
                 return true;
-            if (y == 11 && x >= 2 && x <= 7)
+            if (y == 5 && x == 13)
                 return true;
-            if (y >= 7 && y <= 11 && x == 7)
+            if ((y >= 6 && y <= 8) && (x >= 3 && x <= 7))
                 return true;
-            if (y >= 3 && y <= 5 && x >= 10 && x <= 12)
+            if ((y >= 6 && y <= 8) && x == 13)
                 return true;
-            if (y >= 5 && y <= 8 && x == 13)
+            if ((y == 9 || y == 10) && (x == 5 || x == 7))
+                return true;
+            if (y == 11 && (x >= 2 && x <= 7))
+                return true;
+            if (y == 12 && (x >= 5 && x <= 7))
                 return true;
 
             return false;

052b855f150f2dec6c4e28d53aa8a19c644bbf5a
clounada <clounada@DESKTOP-O6CLMDD>
2025-10-03 19:16:09 +0300

    Add project files.

---
+++ Tyuiu.ShiganovaAV.Sprint2.Task0.V18.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.ShiganovaAV.Sprint2.Task0.V18.Lib;
+namespace Tyuiu.ShiganovaAV.Sprint2.Task0.V18.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetCompareOperation()
+        {
+             DataService ds = new DataService();
+            int x = 5105;
+            int y = 475;
+            bool[]res=new bool[6];
+            bool[] wait = new bool [6] { false, true, true, true, true, false };
+             res = ds.GetCompareOperations(x, y);
+            CollectionAssert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.ShiganovaAV.Sprint2.Task0.V18/Program.cs
@@ -0,0 +1,43 @@
+﻿using Tyuiu.ShiganovaAV.Sprint2.Task0.V18.Lib;
+namespace Tyuiu.ShiganovaAV.Sprint2.Task0.V18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 |Выполнила: Шиганова А.В.| ПИНб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Алгоритмы разветвляющейся структуры                               *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #18                                                             *");
+            Console.WriteLine("* Выполнила: Шиганова Анастасия Владимировна |ПИНб-25-1|                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int x = 5105;
+            Console.WriteLine("х=" + x);
+            int y = 475;
+            Console.WriteLine("y=" + y);
+            bool[] res = new bool[6];
+            res = ds.GetCompareOperations(x, y);
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(res[i]);
+            }
+            Console.ReadKey();
+
+        }
+    }
+
+
+
+}

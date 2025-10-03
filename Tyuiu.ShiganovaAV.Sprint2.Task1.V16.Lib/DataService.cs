81c3654eb0d6d5a1feadb1947148268916d36f14
clounada <Tyuiu.ShiganovaAV@outlook.com>
2025-10-03 20:05:46 +0300

    т

---
+++ Tyuiu.ShiganovaAV.Sprint2.Task1.V16.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.ShiganovaAV.Sprint2.Task1.V16.Lib
+{
+    public class DataService : ISprint2Task1V16
+    {
+        public bool[] GetLogicOperations(int a, int b, int c, int d)
+        {
+            bool[] res = new bool[6];
+             res[0] = (a > c) & (b != d);
+            res[1] = (a == c + 1) | (d < a);
+            res[2] = (b > a) && (c >= d);
+            res[3] = (a <= d) ^ (c == b);
+             res[4] = (b != c) && !(a < d);
+             res[5] = (d >= a) | (c == a);
+             return res;
+        }
+    }
+}
---
+++ Tyuiu.ShiganovaAV.Sprint2.Task1.V16/Program.cs
@@ -0,0 +1,43 @@
+﻿using Tyuiu.ShiganovaAV.Sprint2.Task1.V16.Lib;
+namespace Tyuiu.ShiganovaAV.Sprint2.Task1.V16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 |Выполнила: Шиганова А.В.| ПИНб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Алгоритмы разветвляющейся структуры                               *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #16                                                             *");
+            Console.WriteLine("* Выполнила: Шиганова Анастасия Владимировна |ПИНб-25-1|                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int a = 145;
+            Console.WriteLine("a=" + a);
+            int b = 716;
+            Console.WriteLine("b=" + b);
+            int c = 144;
+            Console.WriteLine("c=" + c);
+            int d = 137;
+            Console.WriteLine("d=" + d);
+            bool[] res = new bool[6];
+            res = ds.GetLogicOperations(a, b,c,d);
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(res[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
+

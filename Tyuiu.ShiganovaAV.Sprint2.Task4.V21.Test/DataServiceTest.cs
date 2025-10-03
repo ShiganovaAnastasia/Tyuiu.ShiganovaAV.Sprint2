cd5eba1688f6c2f0492476c7d14a7d0ed357e835
clounada <Tyuiu.ShiganovaAV@outlook.com>
2025-10-03 22:39:06 +0300

    .

---
+++ Tyuiu.ShiganovaAV.Sprint2.Task4.V21.Test/DataServiceTest.cs
@@ -0,0 +1,45 @@
+﻿using Tyuiu.ShiganovaAV.Sprint2.Task4.V21.Lib;
+namespace Tyuiu.ShiganovaAV.Sprint2.Task4.V21.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double x = 1;
+            double y = 5;
+            double wait = 1024;
+            double res = ds.Calculate(x, y);
+            Assert.AreEqual(wait, res);
+        }
+        public void TestMethod2()
+        {
+            DataService ds = new DataService();
+            double x = 3;
+            double y = 2;
+            double wait = 1.198;
+            double res = ds.Calculate(x, y);
+            Assert.AreEqual(wait, res);
+        }
+        public void TestMethod3()
+        {
+            DataService ds = new DataService();
+            double x = 2;
+            double y = 6;
+            double wait = 434.012;
+            double res = ds.Calculate(x, y);
+            Assert.AreEqual(wait, res);
+        }
+        public void TestMethod4()
+        {
+            DataService ds = new DataService();
+            double x = 0.5;
+            double y = 3;
+            double wait = 512;
+            double res = ds.Calculate(x, y);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.ShiganovaAV.Sprint2.Task4.V21/Program.cs
@@ -0,0 +1,35 @@
+﻿using Tyuiu.ShiganovaAV.Sprint2.Task4.V21.Lib;
+namespace Tyuiu.ShiganovaAV.Sprint2.Task4.V21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 |Выполнила: Шиганова А.В.| ПИНб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Алгоритмы разветвляющейся структуры                               *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #21                                                             *");
+            Console.WriteLine("* Выполнила: Шиганова Анастасия Владимировна |ПИНб-25-1|                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("x=" + x);
+            double y = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("y=" + y);
+
+            double res = ds.Calculate(x, y);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Ответ: " + res);
+            Console.ReadKey();
+        }
+    }
+}
+
